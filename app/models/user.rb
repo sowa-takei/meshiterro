7248444f90905740a5591c47d2a5ba47d60a98c6
sowa-takei <souwasora@gmail.com>
2021-07-27 14:20:56 +0000

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   # 1:Nの関係になるモデル名を複数形で記述します。
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  attachment :profile_image
   
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  
   
 end
